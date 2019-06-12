5958d3566002aabcf69d6393489770f31ba50f34
iankurao <kuraoian@gmail.com>
2019-06-12 14:16:56 +0300

    import project settings and add to the url patterns a new static route that references location of uploaded files

--- news/urls.py
+++ news/urls.py
@@ -6,3 +6,6 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     url(r'^search/', views.search_results, name='search_results')    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
